3c742e9e380caca68984769297f6c4aa73bb375f
Li Haoyi <haoyi.sg@gmail.com>
2024-02-05 17:11:55 +0800

    basic unit tets for scala 2 class

--- unroll/tests/cls/src/UnrollTestMain.scala
+++ unroll/tests/cls/src/UnrollTestMain.scala
@@ -4,22 +4,33 @@ class UnrollTestMain{
 
   def print2(x: Any) = println(x)
 
-  @unroll.Unroll("n")
-  def foo(s: String, n: Int = 1, b: Boolean = true) = print2(s + n + b)
+  @unroll.Unroll("b")
+  def foo(s: String, n: Int = 1, b: Boolean = true, l: Long = 0) = s + n + b + l
 
 }
 
 
 object UnrollTestMain{
   def main(args: Array[String]): Unit = {
-    new UnrollTestMain().foo("hello", 2)
+    val instance = new UnrollTestMain()
+    val cls = classOf[UnrollTestMain]
 
-    var i = 0
-    val methods = classOf[UnrollTestMain].getMethods
-    while (i < methods.length) {
-      if (methods(i).getName.contains("foo")) println(methods(i))
-      i += 1
-    }
+    assert(scala.util.Try(cls.getMethod("foo", classOf[String])).isFailure)
+    println()
+    assert(
+      cls.getMethod("foo", classOf[String], classOf[Int]).invoke(instance, "hello", 2) ==
+      "hello2true0"
+    )
+    assert(
+      cls.getMethod("foo", classOf[String], classOf[Int], classOf[Boolean])
+        .invoke(instance, "hello", 2, false) ==
+      "hello2false0"
+    )
+    assert(
+      cls.getMethod("foo", classOf[String], classOf[Int], classOf[Boolean], classOf[Long])
+        .invoke(instance, "hello", 2, false, 3) ==
+      "hello2false3"
+    )
   }
 }
 
