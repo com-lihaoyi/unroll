9e40037fff6d5cdef7ef827de3f094645adfe3c9
Li Haoyi <haoyi.sg@gmail.com>
2024-02-06 10:31:29 +0800

    add test case for static methods failing on 2.13.11

--- unroll/tests/obj/src/UnrollTestMain.scala
+++ unroll/tests/obj/src/UnrollTestMain.scala
@@ -4,24 +4,45 @@ package unroll
 object UnrollTestMain{
   def main(args: Array[String]): Unit = {
     val instance = Unrolled
-    val cls = classOf[Unrolled.type]
+    val instanceCls = classOf[Unrolled.type]
 
-    cls.getMethods.filter(_.getName.contains("foo")).foreach(println)
 
-    assert(scala.util.Try(cls.getMethod("foo", classOf[String])).isFailure)
-    println()
+    instanceCls.getMethods.filter(_.getName.contains("foo")).foreach(println)
+
+    // Make sure singleton instance forwarder methods are generated
+    assert(scala.util.Try(instanceCls.getMethod("foo", classOf[String])).isFailure)
     assert(
-      cls.getMethod("foo", classOf[String], classOf[Int]).invoke(instance, "hello", 2) ==
+      instanceCls.getMethod("foo", classOf[String], classOf[Int]).invoke(instance, "hello", 2) ==
         "hello2true0"
     )
     assert(
-      cls.getMethod("foo", classOf[String], classOf[Int], classOf[Boolean])
+      instanceCls.getMethod("foo", classOf[String], classOf[Int], classOf[Boolean])
         .invoke(instance, "hello", 2, false) ==
         "hello2false0"
     )
     assert(
-      cls.getMethod("foo", classOf[String], classOf[Int], classOf[Boolean], classOf[Long])
+      instanceCls.getMethod("foo", classOf[String], classOf[Int], classOf[Boolean], classOf[Long])
         .invoke(instance, "hello", 2, false, 3) ==
+        "hello2false3"
+    )
+
+    // Make sure static forwarder methods are generated
+    val staticCls = Class.forName("unroll.Unrolled")
+    staticCls.getMethods.filter(_.getName.contains("foo")).foreach(println)
+
+    assert(scala.util.Try(staticCls.getMethod("foo", classOf[String])).isFailure)
+    assert(
+      staticCls.getMethod("foo", classOf[String], classOf[Int]).invoke(null, "hello", 2) ==
+        "hello2true0"
+    )
+    assert(
+      staticCls.getMethod("foo", classOf[String], classOf[Int], classOf[Boolean])
+        .invoke(null, "hello", 2, false) ==
+        "hello2false0"
+    )
+    assert(
+      staticCls.getMethod("foo", classOf[String], classOf[Int], classOf[Boolean], classOf[Long])
+        .invoke(null, "hello", 2, false, 3) ==
         "hello2false3"
     )
   }
