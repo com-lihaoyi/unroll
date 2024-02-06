c8c28a84789eb9f99c529854cea2036b0c8c94e6
Li Haoyi <haoyi.sg@gmail.com>
2024-02-06 15:53:46 +0800

    cleanup-scala3

--- unroll/src-3/UnrollPhaseScala3.scala
+++ unroll/src-3/UnrollPhaseScala3.scala
@@ -27,67 +27,76 @@ class UnrollPhaseScala3() extends PluginPhase {
   override val runsAfter = Set(transform.Pickler.name)
 
   override def transformTemplate(tmpl: tpd.Template)(using Context): tpd.Tree = {
-    val newMethods = (tmpl.body ++ Seq(tmpl.constr)).collect{
-      case defdef: DefDef =>
-        import dotty.tools.dotc.core.NameOps.isConstructorName
-        val allParams = defdef.paramss.asInstanceOf[List[List[ValDef]]].flatten
+    def copyParam(p: ValDef, parent: Symbol) = {
+      implicitly[Context].typeAssigner.assignType(
+        cpy.ValDef(p)(p.name, p.tpt, p.rhs),
+        Symbols.newSymbol(parent, p.name, p.symbol.flags, p.symbol.info)
+      )
+    }
 
-        val annotated = if (defdef.symbol.isPrimaryConstructor) defdef.symbol.owner else defdef.symbol
+    def potentialDefDefs = (tmpl.body ++ Seq(tmpl.constr)).collect{ case defdef: DefDef if defdef.paramss.nonEmpty => defdef }
 
-        for(annot <- annotated.annotations.find(_.symbol.fullName.toString == "unroll.Unroll")) yield {
-          val Some(Literal(Constant(argName: String))) = annot.argument(0)
+    val newMethods = for(defdef <- potentialDefDefs) yield {
+      import dotty.tools.dotc.core.NameOps.isConstructorName
+      val firstParams :: restParams = defdef.paramss.asInstanceOf[List[List[ValDef]]]
 
-          val argIndex = allParams.indexWhere(_.name.toString == argName)
+      val annotated = if (defdef.symbol.isPrimaryConstructor) defdef.symbol.owner else defdef.symbol
 
-          val prevMethodType = defdef.symbol.info.asInstanceOf[MethodType]
-          for(n <- Range(argIndex, allParams.size)) yield{
-            val truncatedMethodType = MethodType(
-              prevMethodType.paramInfos.take(n),
-              prevMethodType.resType
-            )
+      for (annot <- annotated.annotations.find(_.symbol.fullName.toString == "unroll.Unroll")) yield {
+        val Some(Literal(Constant(argName: String))) = annot.argument(0)
 
-            val newSymbol = Symbols.newSymbol(defdef.symbol.owner, defdef.name, defdef.symbol.flags, truncatedMethodType)
+        val argIndex = firstParams.indexWhere(_.name.toString == argName)
 
-            val paramss = allParams.take(n).map { p =>
-              implicitly[Context].typeAssigner.assignType(
-                cpy.ValDef(p)(defdef.name, p.tpt, p.rhs),
-                Symbols.newSymbol(newSymbol, p.name, p.symbol.flags, p.symbol.info)
-              )
+        val prevMethodType = defdef.symbol.info.asInstanceOf[MethodType]
+        for (n <- Range(argIndex, firstParams.size)) yield {
+          val truncatedMethodType = MethodType(
+            prevMethodType.paramInfos.take(n),
+            prevMethodType.resType
+          )
+
+          val forwarderDefSymbol = Symbols.newSymbol(
+            defdef.symbol.owner,
+            defdef.name,
+            defdef.symbol.flags,
+            truncatedMethodType
+          )
+
+          val newFirstParamss = firstParams.take(n).map(copyParam(_, forwarderDefSymbol))
+          val newRestParamss = restParams.map(_.map(copyParam(_, forwarderDefSymbol)))
+
+          val defaultCalls = for(n <- Range(n, firstParams.size)) yield {
+            if (defdef.symbol.isConstructor) {
+              ref(defdef.symbol.owner.companionModule)
+                .select(DefaultGetterName(defdef.name, n))
+            } else {
+              This(defdef.symbol.owner.asClass)
+                .select(DefaultGetterName(defdef.name, n))
             }
-
-            val applyTree = Apply(
-              This(defdef.symbol.owner.asClass).select(defdef.symbol),
-              paramss.map(p => ref(p.symbol)) ++
-                Range(n, allParams.size).map(n2 =>
-                  if (defdef.symbol.isConstructor){
-                    ref(defdef.symbol.owner.companionModule)
-                      .select(DefaultGetterName(defdef.name, n2))
-                  }else{
-                    This(defdef.symbol.owner.asClass)
-                      .select(DefaultGetterName(defdef.name, n2))
-                  }
-                )
-            )
-
-            val newDefDef = implicitly[Context].typeAssigner.assignType(
-              cpy.DefDef(defdef)(
-                name = newSymbol.name,
-                paramss = List(paramss),
-                tpt = defdef.tpt,
-                rhs =
-                  if (defdef.symbol.isConstructor){
-                    Block(
-                      List(applyTree),
-                      Literal(Constant(()))
-                    )
-                  }else applyTree
-              ),
-              newSymbol
-            )
-
-            newDefDef
           }
+          
+          val allNewParamTrees = 
+            List(newFirstParamss.map(p => ref(p.symbol)) ++ defaultCalls) ++
+            newRestParamss.map(_.map(p => ref(p.symbol)))
+          
+          var applyTree: Tree = This(defdef.symbol.owner.asClass).select(defdef.symbol)
+
+          for (newParams <- allNewParamTrees) applyTree = Apply(applyTree, newParams)
+
+          val newDefDef = implicitly[Context].typeAssigner.assignType(
+            cpy.DefDef(defdef)(
+              name = forwarderDefSymbol.name,
+              paramss = List(newFirstParamss) ++ newRestParamss,
+              tpt = defdef.tpt,
+              rhs =
+                if (!defdef.symbol.isConstructor) applyTree
+                else Block(List(applyTree), Literal(Constant(())))
+            ),
+            forwarderDefSymbol
+          )
+
+          newDefDef
         }
+      }
     }
 
     super.transformTemplate(
