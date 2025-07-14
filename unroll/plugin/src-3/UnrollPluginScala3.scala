1b0270ab70ba1a8ff5b99c828e32b0be40b2d51f
Li Haoyi <haoyi.sg@gmail.com>
2025-07-14 13:02:26 +0800

    3.7.1

--- unroll/plugin/src-3/UnrollPluginScala3.scala
+++ unroll/plugin/src-3/UnrollPluginScala3.scala
@@ -5,5 +5,5 @@ import dotty.tools.dotc.plugins._
 class UnrollPluginScala3 extends StandardPlugin {
   val name: String = "unroll"
   override val description: String = "Count method calls"
-  def init(options: List[String]): List[PluginPhase] = List(new UnrollPhaseScala3())
+  override def init(options: List[String]): List[PluginPhase] = List(new UnrollPhaseScala3())
 }
